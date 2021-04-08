3236188d4afccfc4e80d33006e9013ca8e00d39d
ryancho-hakhyeon <62979514+ryancho-hakhyeon@users.noreply.github.com>
2021-04-07 21:12:13 -0700

    Update app.py

--- receiver/app.py
+++ receiver/app.py
@@ -12,14 +12,27 @@ import datetime
 EVENTS_FILE = "events.json"
 MAX_EVENT = 10
 
-with open('app_conf.yml', 'r') as f:
+if "TARGET_ENV" in os.environ and os.environ["TARGET_ENV"] == "test":
+    print("In Test Environment")
+    app_conf_file = "/config/app_conf.yml"
+    log_conf_file = "/config/log_conf.yml"
+else:
+    print("In Dev Environment")
+    app_conf_file = "app_conf.yml"
+    log_conf_file = "log_conf.yml"
+
+with open(app_conf_file, 'r') as f:
     app_config = yaml.safe_load(f.read())
 
-with open('log_conf.yml', 'r') as f:
+# External Logging Configuration
+with open(log_conf_file, 'r') as f:
     log_config = yaml.safe_load(f.read())
     logging.config.dictConfig(log_config)
 
 logger = logging.getLogger('basicLogger')
+
+logger.info("App Conf File: %s" % app_conf_file)
+logger.info("Log Conf File: %s" % log_conf_file)
 
 
 def log_event(event):
