1fb89e12a96c8805b5e34eb57a11d06cab60a8cc
ryancho-hakhyeon <62979514+ryancho-hakhyeon@users.noreply.github.com>
2021-04-08 01:30:02 -0700

    Update list_category.py

--- storage/list_category.py
+++ storage/list_category.py
@@ -11,14 +11,16 @@ class ListCategory(Base):
     id = Column(Integer, primary_key=True)
     category_id = Column(String(100), nullable=False)
     category_name = Column(String(100), nullable=False)
+    timestamp = Column(String(100), nullable=False)
     images_num = Column(Integer, nullable=False)
     date_created = Column(DateTime, nullable=False)
 
-    def __init__(self, category_id, category_name, images_num):
+    def __init__(self, category_id, category_name, images_num, timestamp,):
         """ Initialize a list category """
         self.category_id = category_id
         self.category_name = category_name
         self.images_num = images_num
+        self.timestamp = timestamp
         self.date_created = datetime.datetime.now()
 
     def to_dict(self):
@@ -27,6 +29,7 @@ class ListCategory(Base):
         dict['category_id'] = self.category_id
         dict['category_name'] = self.category_name
         dict['images_num'] = self.images_num
+        dict['timestamp'] = self.timestamp
         dict['date_created'] = self.date_created
 
         return dict
