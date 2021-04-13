3560f8754ce80cb44d96d8cef0439428ee3f1f74
ryancho-hakhyeon <62979514+ryancho-hakhyeon@users.noreply.github.com>
2021-04-13 11:25:49 -0700

    Update app.py

--- audit_log/app.py
+++ audit_log/app.py
@@ -91,7 +91,7 @@ def get_list_category_reading(index):
 
 
 app = connexion.FlaskApp(__name__, specification_dir='')
-app.add_api("openapi.yaml", strict_validation=True, validate_responses=True)
+app.add_api("openapi.yaml", base_path="/audit_log", strict_validation=True, validate_responses=True)
 CORS(app.app)
 app.app.config['CORS_HEADERS']='Content-Type'
 
