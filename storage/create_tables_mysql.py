f2fc1e3b136edcf80249bbac4064f5da3da25706
ryancho-hakhyeon <62979514+ryancho-hakhyeon@users.noreply.github.com>
2021-06-09 20:29:43 -0700

    Update create_tables_mysql.py

--- storage/create_tables_mysql.py
+++ storage/create_tables_mysql.py
@@ -1,8 +1,19 @@
 import mysql.connector
 import yaml
 
-with open('app_conf.yml', 'r') as f:
+if "TARGET_ENV" in os.environ and os.environ["TARGET_ENV"] == "test":
+    print("In Test Environment")
+    app_conf_file = "/config/app_conf.yml"
+else:
+    print("In Dev Environment")
+    app_conf_file = "app_conf.yml"
+
+with open(app_conf_file, 'r') as f:
     app_config = yaml.safe_load(f.read())
+
+
+#with open('app_conf.yml', 'r') as f:
+#    app_config = yaml.safe_load(f.read())
 
 db_conn = mysql.connector.connect(host=app_config["datastore"]["hostname"],
                                   user=app_config["datastore"]["user"],
@@ -10,13 +21,14 @@ db_conn = mysql.connector.connect(host=app_config["datastore"]["hostname"],
                                   port=app_config["datastore"]["port"],
                                   database=app_config["datastore"]["db"])
 
-#db_conn = mysql.connector.connect(host="localhost", user="root", password="password", database="events")
+#db_conn = mysql.connector.connect
+#(host="localhost", user="root", password="password", database="events")
 
 db_cursor = db_conn.cursor()
 db_cursor.execute('''
                 CREATE TABLE crawling_image(
-                    id INT NOT NULL AUTO_INCREMENT, 
-                    image_id VARCHAR(100) NOT NULL, 
+                    id INT NOT NULL AUTO_INCREMENT,
+                    image_id VARCHAR(100) NOT NULL,
                     image_name VARCHAR(100) NOT NULL,
                     date_created VARCHAR(100) NOT NULL,
                     timestamp VARCHAR(100) NOT NULL,
